3cfdb3e348cd230466fc383f8ca8a8970c7707c8
Jay.Udey <jay.udey@webfilings.com>
2016-06-27 13:24:45 -0500

    adding tests for new check param

--- test/integration/gen_test_runner_test.dart
+++ test/integration/gen_test_runner_test.dart
@@ -22,15 +22,19 @@ import 'package:dart_dev/util.dart' show TaskProcess;
 import 'package:path/path.dart' as path;
 import 'package:test/test.dart';
 
-const String defaultConfig = 'test/fixtures/gen_test_runner/default_config';
-const String browserAndVm = 'test/fixtures/gen_test_runner/browser_and_vm';
+const String browserAndVm = 'test_fixtures/gen_test_runner/browser_and_vm';
+const String checkFail = 'test_fixtures/gen_test_runner/check_fail';
+const String checkPass = 'test_fixtures/gen_test_runner/check_pass';
+const String defaultConfig = 'test_fixtures/gen_test_runner/default_config';
 
-Future<Runner> generateTestRunnerDocsFor(String projectPath,
+Future<Runner> generateTestRunner(String projectPath,
     {List<String> additionalArgs: const []}) async {
   await Process.run('pub', ['get'], workingDirectory: projectPath);
 
   var files = [];
   var errors = [];
+  var stderr = '';
+  var stdout = '';
   var args = ['run', 'dart_dev', 'gen-test-runner'];
   if (additionalArgs.isNotEmpty) {
     additionalArgs.forEach((argument) {
@@ -40,9 +44,17 @@ Future<Runner> generateTestRunnerDocsFor(String projectPath,
   TaskProcess process =
       new TaskProcess('pub', args, workingDirectory: projectPath);
 
+  process.stdout.listen((line) {
+    stdout += line;
+  });
+
+  process.stderr.listen((line) {
+    stderr += line;
+  });
+
   await process.done;
 
-  return new Runner(await process.exitCode, errors, files);
+  return new Runner(await process.exitCode, errors, files, stderr, stdout);
 }
 
 verifyExistenceAndCleanup(String file,
@@ -56,7 +68,7 @@ verifyExistenceAndCleanup(String file,
 void main() {
   group('gen-test-runner task', () {
     test('should work with default config', () async {
-      Runner runner = await generateTestRunnerDocsFor(defaultConfig);
+      Runner runner = await generateTestRunner(defaultConfig);
       expect(runner.exitCode, isZero);
       verifyExistenceAndCleanup(
           path.join(defaultConfig, 'test/generated_runner.dart'),
@@ -67,7 +79,7 @@ void main() {
     });
 
     test('should work with multiple configs', () async {
-      Runner runner = await generateTestRunnerDocsFor(browserAndVm);
+      Runner runner = await generateTestRunner(browserAndVm);
       expect(runner.exitCode, isZero);
       verifyExistenceAndCleanup(
           path.join(browserAndVm, 'test/browser/generated_runner.dart'),
@@ -84,30 +96,23 @@ void main() {
     });
 
     group('--check flag', () {
-      test('should succeed if the runner was generated', () async {
-        Runner runner = await generateTestRunnerDocsFor(defaultConfig);
+      test('should succeed if the runner is up to date', () async {
+        Runner runner =
+            await generateTestRunner(checkPass, additionalArgs: ['--check']);
         expect(runner.exitCode, isZero);
-        Runner checkRunner = await generateTestRunnerDocsFor(defaultConfig,
-            additionalArgs: ['--check']);
-        expect(checkRunner.exitCode, isZero);
-        verifyExistenceAndCleanup(
-            path.join(defaultConfig, 'test/generated_runner.dart'),
-            shouldFileExist: true);
-        verifyExistenceAndCleanup(
-            path.join(defaultConfig, 'test/generated_runner.html'),
-            shouldFileExist: false);
+        expect(runner.stderr, equals(''));
+        expect(runner.stdout.contains('Generated test runner is up-to-date.'),
+            isTrue);
       });
 
-      test('should fail if the runner was not generated', () async {
-        Runner runner = await generateTestRunnerDocsFor(defaultConfig,
-            additionalArgs: ['--check']);
+      test('should fail if the runner is not up to date', () async {
+        Runner runner =
+            await generateTestRunner(checkFail, additionalArgs: ['--check']);
         expect(runner.exitCode, isNot(0));
-        verifyExistenceAndCleanup(
-            path.join(defaultConfig, 'test/generated_runner.dart'),
-            shouldFileExist: true);
-        verifyExistenceAndCleanup(
-            path.join(defaultConfig, 'test/generated_runner.html'),
-            shouldFileExist: false);
+        expect(
+            runner.stderr.contains('Generated test runner is not up-to-date.'),
+            isTrue);
+        expect(runner.stdout, equals(''));
       });
     });
   });
@@ -117,5 +122,8 @@ class Runner {
   final int exitCode;
   final List<String> errors;
   final List<String> files;
-  Runner(this.exitCode, this.errors, this.files);
+  final String stderr;
+  final String stdout;
+
+  Runner(this.exitCode, this.errors, this.files, this.stderr, this.stdout);
 }
